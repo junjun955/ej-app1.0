c37546bd0b82a0a7d47cad07339ec0e2607f461c
yuchendegithub <51306902+yuchendegithub@users.noreply.github.com>
2019-06-19 18:25:01 +0800

    app的MyPage界面

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -1,11 +1,59 @@
 import React from 'react'
+import { Tabs, WhiteSpace, Badge } from 'antd-mobile';
+import styles from './MyPage.css'
+
+const tabs = [
+  { title: <Badge dot>全部订单</Badge> },
+  { title: <Badge dot>待付款</Badge> },
+  { title: <Badge dot>待服务</Badge> },
+  { title: <Badge dot>待评价</Badge> },
+];
+
 
 class MyPage extends React.Component {
 
   render(){
+    const TabExample = () => (
+     <div>
+       
+     </div>
+    );
     return (
       <div>
-        我的
+        <div>
+        <Tabs tabs={tabs}
+          initialPage={1}
+          onChange={(tab, index) => { console.log('onChange', index, tab); }}
+          onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+        >
+          <div className={styles.tab} style={{  alignItems: 'center',  justifyContent: 'center' }}>
+              <br></br>
+              <br></br>
+              <br></br>
+          <font face="verdana" size="6" > 全部订单</font>
+          </div>
+          <div className={styles.tab} style={{  alignItems: 'center', justifyContent: 'center' }}>
+          <br></br>
+              <br></br>
+              <br></br>
+          <font face="verdana" size="6" > 待付款订单</font>
+          </div>
+          <div className={styles.tab} style={{  alignItems: 'center', justifyContent: 'center' }}>
+          <br></br>
+              <br></br>
+              <br></br>
+          <font face="verdana" size="6" > 待服务订单</font>
+          </div>
+          <div className={styles.tab} style={{  alignItems: 'center', justifyContent: 'center'}}>
+          <br></br>
+              <br></br>
+              <br></br>
+          <font face="verdana" size="6" > 待评价订单</font>
+          </div>
+        </Tabs>
+        <WhiteSpace />
+        
+      </div>
 
       </div>
     )
