9a560b88b550dbb9201cc61c800ac8da5fbe2798
lululu0 <1272741817@qq.com>
2019-06-20 10:42:50 +0800

    吕凯璐 完善商品浏览页面

--- src/routes/OrderPage.js
+++ src/routes/OrderPage.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { SearchBar, WhiteSpace, WingBlank } from 'antd-mobile';
+import { List,Tabs,SearchBar, WhiteSpace, WingBlank, Stepper  } from 'antd-mobile';
 import { connect } from 'dva';
 import styles from './OrderPage.css'
 import axios from '../utils/axios'
 
+
+const tabs = [
+  { title: '洗护', key: 't1' },
+  { title: '保洁', key: 't2' },
+  { title: '看护', key: 't3' },
+  { title: '月嫂', key: 't3' },
+  { title: '其他', key: 't3' },
+];
 class OrderPage extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      orders:[]
+      orders:[],
     }
   }
-
   // 当前组件绑定到根组件上执行【生命周期钩子】
   // 在生命周期钩子函数中调用重载数据
   componentDidMount(){
     this.reloadData();
   }
-
   // 重载数据
   reloadData(){
     axios.get('/product/findAll')
@@ -28,33 +34,44 @@ class OrderPage extends React.Component {
       })
     });
   }  
-  
   render(){
     return (
-      <div>
-        <div>
-        <WingBlank><div className="sub-title"></div></WingBlank>
+      <div  className={styles["widow"]}>
+     <div>
+     <div>
+      <WingBlank><div className="sub-title"></div></WingBlank>
       <SearchBar placeholder="自动获取光标" ref={ref => this.autoFocusInst = ref} />
       <WhiteSpace />
       </div>
-        {/* 栏目 */}
-        <div>
+    <WhiteSpace />
+    <Tabs tabs={tabs}
+      initialPage={'t1'}
+      tabBarPosition="left"
+      tabDirection="vertical"
+    >
+      
+      <div>
           <ul className={styles["category_list"]}>
             {
               this.state.orders.map((item)=>{
-                return (
-                  <li 
-                   className={styles["category_list_item"]}>
-                     <div> <img width={60} height={60} src={"http://134.175.154.93:8888/group1/"+item.photo}/> 
-                    {item.name+' \n '}{item.price+' \n '}
-                    {item.description} 
-                    </div> 
-                  </li>
+                return (            
+       <List>
+            <li 
+            className={styles["category_list_item"]}>
+                   <div> <img width={50} height={50} src={"http://134.175.154.93:8888/group1/"+item.photo}/> 
+                    {item.name+' \r '}
+                    {"RMB："+item.price}
+                   </div> 
+            </li>      
+      </List>                  
                 )
               })
             }
           </ul>
         </div>
+    </Tabs>
+    <WhiteSpace />
+  </div>        
       </div>
     );
   }
