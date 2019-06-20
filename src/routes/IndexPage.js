e91c0eb5a864d3f5334b3d266579701a93a8db04
junjun955 <529665956@qq.com>
2019-06-20 11:56:28 +0800

    首页加上特效

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -6,95 +6,120 @@ import k from "../assets/kanhu2.png"
 import x from "../assets/xihu3.png"
 import y from "../assets/yuesao1.png"
 import q from "../assets/Donute.png"
-
 import xiyi from "../assets/xiyi.png"
 import youyanji from "../assets/youyanji.png"
-
-
 import styles from './IndexPage.css'
 import axios from '../utils/axios'
-import { Grid  } from 'antd-mobile';
+import { Grid, Carousel, WingBlank } from 'antd-mobile';
 
-
-
-const data = [{
-  icon: x,  
+const data1 = [{
+  icon: x,
   text: `洗护`
-},{
-  icon: b,  
+}, {
+  icon: b,
   text: `保洁`
-},{
-  icon: k,  
+}, {
+  icon: k,
   text: `看护`
-},{
-  icon: y,  
+}, {
+  icon: y,
   text: `月嫂`
-},{
-  icon: q,  
+}, {
+  icon: q,
   text: `其他`
 }
 ]
 
-
-const data1 = [{
-  icon: xiyi,  
+const data2 = [{
+  icon: xiyi,
   text: `洗衣`
-},{
-  icon: youyanji,  
+}, {
+  icon: youyanji,
   text: `清洗油烟机`
-},{
-  icon: '',  
+}, {
+  icon: '',
   text: `更多热门服务`
 }
 ]
 
 class IndexPage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      categories:[]
+      data: ['1', '2', '3'],
+      imgHeight: 176,
+      categories: []
     }
   }
   // 当前组件绑定到根组件上执行【生命周期钩子】
-  componentDidMount(){
+  componentDidMount() {
     this.loadCategory();
-  }
-  loadCategory(){
-    axios.get('/category/findAll')
-    .then((result)=>{
-      // 将数据设置到局部状态中
-      this.setState({
-        categories:result.data
-      })
-    });
-  }
 
-  toProduct(){
+// simulate img loading
+setTimeout(() => {
+  this.setState({
+    data: ['M00/00/05/rBAACV0LAhuADeCdAAdjf5JGOGs255', 'M00/00/05/rBAACV0K47GAPhtTAASkPRP1KkQ709', 'M00/00/05/rBAACV0K482AYg4DAAgB1zLG6xY278'],
+  });
+}, 100);
+
+  }
+  loadCategory() {
+    axios.get('/category/findAll')
+      .then((result) => {
+        // 将数据设置到局部状态中
+        this.setState({
+          categories: result.data
+        })
+      });
+  }
+  toProduct() {
     this.props.history.push("/product");
   }
-
-  render(){
+  render() {
     return (
-          
-        <div>
-        
-        {/* 图片广告 */}
-        <div className={styles.photoWall}>
-          <img className={styles.photo} src={t} alt="加载失败"/>
-        </div>
-    {/* 栏目 */}
-    <div className="sub-title1"></div>
-           <Grid data={data} isCarousel onClick={this.toProduct.bind(this)} 
+      <div>
+        <Carousel
+          autoplay={true}
+          infinite
+          beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+          afterChange={index => console.log('slide to', index)}
+        >
+          {this.state.data.map(val => (
+            <a
+              key={val}
+              href="http://localhost:8000/#/order"
+              style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+            >
+              <img
+                src={`http://134.175.154.93:8888/group1/${val}.png`}
+                alt=""
+                style={{ width: '100%', verticalAlign: 'top' }}
+                onLoad={() => {
+                  // fire window resize event to change height
+                  window.dispatchEvent(new Event('resize'));
+                  this.setState({ imgHeight: 'auto' });
+                }}
+              />
+            </a>
+          ))}
+        </Carousel>
 
-/>
+        {/* 图片广告 */}
+        {/* <div className={styles.photoWall}>
+          <img className={styles.photo} src={t} alt="加载失败"/>
+        </div> */}
+        {/* 栏目 */}
+        <div className="sub-title1"></div>
+        <Grid data={data1} isCarousel onClick={this.toProduct.bind(this)}
+
+        />
         {/* 产品 */}
         <div>
-        <div className="sub-title2"></div>
-          <Grid data={data1} columnNum={3} itemStyle={{ height: '80px', 
-
-background: 'rgba(0,0,0,.05)' }} />
-
-
+          <div className="sub-title2"></div>
+          <Grid data={data2} columnNum={3} itemStyle={{
+            height: '80px',
+            background: 'rgba(0,0,0,.05)'
+          }} />
         </div>
       </div>
     );
